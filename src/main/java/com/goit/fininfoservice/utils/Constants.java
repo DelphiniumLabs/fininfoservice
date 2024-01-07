34d7b70b15ad2fd07a02d5a69071fdfcb5a9ea8f
Dmytro Milovanov <milovanovu@gmail.com>
2024-01-07 09:47:23 +0100

    pom.xml: replaced <spring-boot-starter-web> with <spring-boot-starter> to exclude running Tomcat added .utils package with Constants.java with some String inside

---
+++ src/main/java/com/goit/fininfoservice/utils/Constants.java
@@ -0,0 +1,18 @@
+package com.goit.fininfoservice.utils;
+
+public class Constants {
+    public static final String GREETING = """ 
+            Hello my dear friend! I am The Financial Information Bot.
+            I will keep you aware of changing currency exchange rates.
+            """;
+    public static final String UNKNOWN_COMMAND_TEXT = """
+            Oops! Unknown command entered! Be carefully with drinks!"
+            """;
+    public static final String HELP_TEXT= """
+            Please read instruction before using our amazing bot!
+            This is list of commands you can use in this bot:
+            /start - starts bot
+            /stop - stops bot
+            """;
+
+}
